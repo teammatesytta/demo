faca0047227c49adef575e9413acedc2c7266d34
Tiffany <tiffanyisaprogrammer@gmail.com>
2026-07-24 07:38:46 +0700

    trial2

--- app.js
+++ app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/hello', (req, res) => {
+  res.send('Hello World!!!!!!!');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
